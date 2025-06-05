9fbd37dc87125577285f345cf51c6421a6384a21
Serhii <mashchenko.serhii@lll.kpi.ua>
2025-06-05 18:30:00 +0300

    Update lab_4_1_mutex.c

--- lab_4_1_mutex.c
+++ lab_4_1_mutex.c
@@ -1,95 +1,53 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <string.h>
-#include <time.h>
 
-#define FILENAME "shared_file.txt"
-#define MAX_LEN 100
+#define NUM_ITERATIONS 5
 
-pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
+int shared_counter = 0;
+pthread_mutex_t mutex;
 
-// Багатопотокова версія
-void* writer(void* arg) {
-    for (int i = 0; i < 1000; i++) {
-        pthread_mutex_lock(&file_mutex);
-        FILE* file = fopen(FILENAME, "a");
-        if (file) {
-            fprintf(file, "Line %d\n", i);
-            fclose(file);
-        }
-        pthread_mutex_unlock(&file_mutex);
+void* increment_thread(void* arg) {
+    for (int i = 0; i < NUM_ITERATIONS; i++) {
+        pthread_mutex_lock(&mutex);
+        int temp = shared_counter;
+        temp++;
+        sleep(1);  // Імітуємо тривалу операцію
+        shared_counter = temp;
+        printf("Increment thread: counter = %d\n", shared_counter);
+        pthread_mutex_unlock(&mutex);
+        sleep(1);
     }
     return NULL;
 }
 
-void* reader(void* arg) {
-    for (int i = 0; i < 1000; i++) {
-        pthread_mutex_lock(&file_mutex);
-        FILE* file = fopen(FILENAME, "r");
-        if (file) {
-            char buffer[MAX_LEN];
-            while (fgets(buffer, MAX_LEN, file)) {}
-            fclose(file);
-        }
-        pthread_mutex_unlock(&file_mutex);
+void* decrement_thread(void* arg) {
+    for (int i = 0; i < NUM_ITERATIONS; i++) {
+        pthread_mutex_lock(&mutex);
+        int temp = shared_counter;
+        temp--;
+        sleep(1);  // Імітуємо тривалу операцію
+        shared_counter = temp;
+        printf("Decrement thread: counter = %d\n", shared_counter);
+        pthread_mutex_unlock(&mutex);
+        sleep(1);
     }
     return NULL;
-}
-
-// Однопотокова версія
-void single_thread_file_ops() {
-    for (int i = 0; i < 1000; i++) {
-        FILE* file = fopen(FILENAME, "a");
-        if (file) {
-            fprintf(file, "Line %d\n", i);
-            fclose(file);
-        }
-        
-        file = fopen(FILENAME, "r");
-        if (file) {
-            char buffer[MAX_LEN];
-            while (fgets(buffer, MAX_LEN, file)) {}
-            fclose(file);
-        }
-    }
 }
 
 int main() {
-    clock_t start, end;
-    double cpu_time_used;
+    pthread_t t1, t2;
     
-    // Очистимо файл перед тестами
-    FILE* file = fopen(FILENAME, "w");
-    if (file) fclose(file);
-
-    // Тестуємо багатопотокову версію
-    start = clock();
+    pthread_mutex_init(&mutex, NULL);
     
-    pthread_t writer1, writer2, reader1;
-    pthread_create(&writer1, NULL, writer, NULL);
-    pthread_create(&writer2, NULL, writer, NULL);
-    pthread_create(&reader1, NULL, reader, NULL);
+    pthread_create(&t1, NULL, increment_thread, NULL);
+    pthread_create(&t2, NULL, decrement_thread, NULL);
     
-    pthread_join(writer1, NULL);
-    pthread_join(writer2, NULL);
-    pthread_join(reader1, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
     
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Multithreaded time: %.4f seconds\n", cpu_time_used);
-
-    // Очистимо файл перед наступним тестом
-    file = fopen(FILENAME, "w");
-    if (file) fclose(file);
-
-    // Тестуємо однопотокову версію
-    start = clock();
-    single_thread_file_ops();
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Single-threaded time: %.4f seconds\n", cpu_time_used);
-
-    pthread_mutex_destroy(&file_mutex);
+    pthread_mutex_destroy(&mutex);
+    
+    printf("Final counter value: %d\n", shared_counter);
     return 0;
 }
