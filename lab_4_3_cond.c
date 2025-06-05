8f56c43ff96ae6a8573938c74f645de1b86806cb
Serhii <mashchenko.serhii@lll.kpi.ua>
2025-06-05 18:30:40 +0300

    Update lab_4_3_cond.c

--- lab_4_3_cond.c
+++ lab_4_3_cond.c
@@ -1,83 +1,42 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <time.h>
+#include <unistd.h>
 
-#define BUFFER_SIZE 10
-#define NUM_ITEMS 10000
-
-int buffer[BUFFER_SIZE];
-int count = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_producer = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond_consumer = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int condition = 0;
 
-// Багатопотокова версія з синхронізацією
-void* producer_sync(void* arg) {
-    for (int i = 0; i < NUM_ITEMS; i++) {
-        pthread_mutex_lock(&mutex);
-        while (count == BUFFER_SIZE) {
-            pthread_cond_wait(&cond_producer, &mutex);
-        }
-        buffer[count++] = i;
-        pthread_cond_signal(&cond_consumer);
-        pthread_mutex_unlock(&mutex);
+void* worker_thread(void* arg) {
+    printf("Worker thread started\n");
+    
+    pthread_mutex_lock(&mutex);
+    while (condition == 0) {
+        printf("Worker waiting for condition...\n");
+        pthread_cond_wait(&cond, &mutex);
     }
+    printf("Worker condition met!\n");
+    pthread_mutex_unlock(&mutex);
+    
     return NULL;
-}
-
-void* consumer_sync(void* arg) {
-    for (int i = 0; i < NUM_ITEMS; i++) {
-        pthread_mutex_lock(&mutex);
-        while (count == 0) {
-            pthread_cond_wait(&cond_consumer, &mutex);
-        }
-        int item = buffer[--count];
-        pthread_cond_signal(&cond_producer);
-        pthread_mutex_unlock(&mutex);
-    }
-    return NULL;
-}
-
-// Однопотокова версія
-void single_thread_buffer() {
-    for (int i = 0; i < NUM_ITEMS; i++) {
-        while (count == BUFFER_SIZE) {} // Очікування
-        buffer[count++] = i;
-        
-        while (count == 0) {} // Очікування
-        int item = buffer[--count];
-    }
 }
 
 int main() {
-    clock_t start, end;
-    double cpu_time_used;
-    pthread_t prod, cons;
-
-    // Тест 1: Багатопотокова з синхронізацією
-    count = 0;
-    start = clock();
+    pthread_t worker;
     
-    pthread_create(&prod, NULL, producer_sync, NULL);
-    pthread_create(&cons, NULL, consumer_sync, NULL);
-    pthread_join(prod, NULL);
-    pthread_join(cons, NULL);
+    pthread_create(&worker, NULL, worker_thread, NULL);
     
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Multithreaded sync: %.4f seconds\n", cpu_time_used);
-
-    // Тест 2: Однопотокова версія
-    count = 0;
-    start = clock();
-    single_thread_buffer();
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Single-threaded: %.4f seconds\n", cpu_time_used);
-
+    sleep(3);  // Імітуємо роботу основного потоку
+    
+    pthread_mutex_lock(&mutex);
+    condition = 1;
+    printf("Main thread signaling condition\n");
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
+    
+    pthread_join(worker, NULL);
+    
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond_producer);
-    pthread_cond_destroy(&cond_consumer);
-
+    pthread_cond_destroy(&cond);
+    
     return 0;
 }
